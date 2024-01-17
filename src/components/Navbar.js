dc9056a5783e3de904e317873e95dd76d42a39ca
Dominik Loncar <dominikloncar@outlook.com>
2024-01-17 14:46:27 +0100

    Imported gatsby Link. Added ul with gatsby links for navbar elements.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Homepage</Link>
+        </li>
+        <li>
+          <Link to="/">Recipes</Link>
+        </li>
+        <li>
+          <Link to="/">Community</Link>
+        </li>
+        <li>
+          <Link to="/">Saved recipes</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
