97781741c56c7f40bff22d1b9cb082e2a4817120
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 00:50:40 +0100

    Added tags prop as an array. Displayed tags as span elements. Changed span to div for completion time and difficulty.

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,12 +4,18 @@ export default function Card({
   title = 'Lorem Ipsum',
   completionTimeInMinutes = 60,
   difficulty = 1,
+  tags = [],
 }) {
   return (
     <div>
       <h3>{title}</h3>
       <div>
-        <span>{completionTimeInMinutes}</span> <span>{difficulty}</span>
+        <div>{completionTimeInMinutes}</div> <div>{difficulty}</div>
+      </div>
+      <div>
+        {tags.map(tag => (
+          <span>{tag}</span>
+        ))}
       </div>
     </div>
   );
