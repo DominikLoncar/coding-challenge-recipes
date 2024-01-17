8da7ec64dc174d07d0c72dac0238c7d0b1c097e1
Dominik Loncar <dominikloncar@outlook.com>
2024-01-17 18:05:08 +0100

    Changed span to div in two Logo jsx elements.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Logo() {
   return (
     <div className="bg-black text-white">
-      <span>
+      <div>
         <strong>Meal Deal</strong>
-      </span>
-      <span>Recipes for healthy life</span>
+      </div>
+      <div>Recipes for healthy life</div>
     </div>
   );
 }
