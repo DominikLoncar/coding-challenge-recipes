0b9a41c03339e7b8f30a6bfda58d350f219f6d0e
Dominik Loncar <dominikloncar@outlook.com>
2024-01-17 23:56:42 +0100

    Added more button styling. Text color to white, horizontal padding to 6, vertical padding to 2, medium rounded button borders.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Button({ text = 'Button', link = '' }) {
-  return <button className="bg-light-green">{text}</button>;
+  return (
+    <button className="bg-light-green text-white px-6 py-2 rounded-md">
+      {text}
+    </button>
+  );
 }
