53575f0e905a346c1dbd0a5cf23bc4aeec51a440
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 00:17:35 +0100

    Separated Hero into 2 grid columns. Added img as second column.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,15 +4,20 @@ import { Title, Button } from './index';
 export default function Hero() {
   return (
     <div>
-      <Title>
-        <strong>
-          Poached eggs with
-          <br />
-          broccoli and avocado
-        </strong>
-      </Title>
-      <div className="my-5" />
-      <Button text="View Recipe" />
+      <div className="grid grid-cols-2">
+        <div>
+          <Title>
+            <strong>
+              Poached eggs with
+              <br />
+              broccoli and avocado
+            </strong>
+          </Title>
+          <div className="my-5" />
+          <Button text="View Recipe" />
+        </div>
+        <img />
+      </div>
     </div>
   );
 }
