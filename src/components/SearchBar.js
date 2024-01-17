d37b78c6e8b244d7c06532042398687bdb60a614
Dominik Loncar <dominikloncar@outlook.com>
2024-01-17 22:05:31 +0100

    Created SearchBar component.

---
+++ src/components/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SearchBar() {
+  return (
+    <div>
+      <input type="text" id="search-recipes" placeholder="Search recipes..." />
+    </div>
+  );
+}
