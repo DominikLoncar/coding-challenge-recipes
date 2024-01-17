b034349fbaa30e5ad5e368788d2a31cb41a06fa9
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 00:00:51 +0100

    Added dm-sans font.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Button({ text = 'Button', link = '' }) {
   return (
-    <button className="bg-light-green text-white px-6 py-2 rounded-md hover:text-light-green hover:bg-white hover:shadow-lg transition-hover transition-colors transition-shadow">
+    <button className="font-dm-sans bg-light-green text-white px-6 py-2 rounded-md hover:text-light-green hover:bg-white hover:shadow-lg transition-hover transition-colors transition-shadow">
       {text}
     </button>
   );
