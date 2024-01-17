15a0356a68ebd23f04a1a338e0c452e81c78b021
Dominik Loncar <dominikloncar@outlook.com>
2024-01-17 23:38:30 +0100

    Added proper Title and Button.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,5 +2,10 @@ import React from 'react';
 import { Title, Button } from './index';
 
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <Title text="Poached eggs with broccoli and avocado" />
+      <Button text="View Recipe" />
+    </div>
+  );
 }
