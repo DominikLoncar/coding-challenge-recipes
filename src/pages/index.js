dde28fe08922cb84208921d27aeca441e4b3d912
Dominik Loncar <dominikloncar@outlook.com>
2024-01-17 18:07:29 +0100

    Imported and displayed Title component on the homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import Navbar from '../components/Navbar';
 import Logo from '../components/Logo';
+import Title from '../components/Title';
 
 const IndexPage = () => {
   return (
     <main>
       <Logo />
       <Navbar />
+      <Title />
     </main>
   );
 };
