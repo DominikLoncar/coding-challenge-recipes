9fb0adcea5217d2e1a24f64f468110c3bbb842bf
Dominik Loncar <dominikloncar@outlook.com>
2024-01-17 18:03:51 +0100

    Added basic Logo component structure.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default function Logo() {
-  return <div></div>;
+  return (
+    <div className="bg-black text-white">
+      <span>
+        <strong>Meal Deal</strong>
+      </span>
+      <span>Recipes for healthy life</span>
+    </div>
+  );
 }
