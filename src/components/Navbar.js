59a01b491ace9a8bca5e3121254da753aebe2889
Dominik Loncar <dominikloncar@outlook.com>
2024-01-17 22:53:47 +0100

    Imported NavbarItem and displayed it. Added navbarItems array of objects to easily add or remove navbar items.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import NavbarItem from './NavbarItem';
 
 export default function Navbar() {
+  const navbarItems = [
+    {
+      link: 'homepage',
+      text: 'Homepage',
+    },
+    {
+      link: 'recipes',
+      text: 'Recipes',
+    },
+    {
+      link: 'community',
+      text: 'Community',
+    },
+    {
+      link: 'saved-recipes',
+      text: 'Saved recipes',
+    },
+    {
+      link: 'settings',
+      text: 'Settings',
+    },
+  ];
+
   return (
     <div className="bg-light-gray text-dark-gray">
       <ul className="flex flex-col gap-6">
+        <NavbarItem link="" />
         <li className="hover:text-dark-green">
           <Link to="/">Homepage</Link>
         </li>
--- src/components/NavbarItem.js
+++ src/components/NavbarItem.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-export default function NavbarItem() {
-  return <div></div>;
+export default function NavbarItem({ link = '', text = 'Lorem' }) {
+  const linkWithSlash = `/${link}`;
+
+  return (
+    <li className="hover:text-dark-green">
+      <Link to={linkWithSlash}>{text}</Link>
+    </li>
+  );
 }
