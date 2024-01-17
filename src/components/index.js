b5bdc6ebd8e51a000b0c0e4cfe3ae834286bf6fb
Dominik Loncar <dominikloncar@outlook.com>
2024-01-17 23:22:10 +0100

    Added exports for Logo, Navbar and NavbarItem.

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Logo } from './Logo';
+export { default as Navbar } from './Navbar';
+export { default as NavbarItem } from './NavbarItem';
+export { default as Title } from './TitleAndSearchBar';
+export { default as SearchBar } from './TitleAndSearchBar';
+export { default as TitleAndSearchBar } from './TitleAndSearchBar';
