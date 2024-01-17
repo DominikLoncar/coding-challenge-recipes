acd77616f84c630669464b723c84b696a5b727cb
Dominik Loncar <dominikloncar@outlook.com>
2024-01-17 23:18:30 +0100

    Updated TitleAndSearchBar component by adding grid, 2 columns and importing and displaying Title and SearchBar together.

--- src/components/TitleAndSearchBar.js
+++ src/components/TitleAndSearchBar.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import Title from './Title';
+import SearchBar from './SearchBar';
 
 export default function TitleAndSearchBar() {
-  return <div>TitleAndSearchBar</div>;
+  return (
+    <div className="grid grid-cols-2">
+      <Title />
+      <SearchBar />
+    </div>
+  );
 }
