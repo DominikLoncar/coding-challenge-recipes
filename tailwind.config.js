d270f9df634444978e50baf6b86462adde2749c2
Dominik Loncar <dominikloncar@outlook.com>
2024-01-17 22:28:55 +0100

    Added gray color into tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: '#F4F5F6',
+      },
+    },
   },
   plugins: [],
-}
+};
