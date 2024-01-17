af1f9a0e3609cee01660bfb824127ac6013c787b
Dominik Loncar <dominikloncar@outlook.com>
2024-01-17 23:41:42 +0100

    Fixed bug for the SearchBar export.

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export { default as Logo } from './Logo';
 export { default as Navbar } from './Navbar';
 export { default as NavbarItem } from './NavbarItem';
 export { default as Title } from './Title';
-export { default as SearchBar } from './TitleAndSearchBar';
+export { default as SearchBar } from './SearchBar';
 export { default as TitleAndSearchBar } from './TitleAndSearchBar';
 export { default as Button } from './Button';
 export { default as Hero } from './Hero';
