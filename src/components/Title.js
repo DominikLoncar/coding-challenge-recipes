f8ba6cbdd21672e1137590b9899d3fa2c456cb2e
Dominik Loncar <dominikloncar@outlook.com>
2024-01-17 23:28:53 +0100

    Changed title text color to midnight-blue.

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Title({ text = 'Lorem Ipsum' }) {
-  return <h1 className="text-4xl font-dm-sans">{text}</h1>;
+  return <h1 className="text-4xl text-midnight-blue font-dm-sans">{text}</h1>;
 }
