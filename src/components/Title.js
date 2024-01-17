df08a964946d5873ecafe5417e599ab46c01ac45
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 00:04:08 +0100

    Passed children prop instead of text prop.

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
-export default function Title({ text = 'Lorem Ipsum' }) {
-  return <h1 className="text-4xl text-midnight-blue font-dm-sans">{text}</h1>;
+export default function Title({ children = 'Lorem Ipsum' }) {
+  return (
+    <h1 className="text-4xl text-midnight-blue font-dm-sans">{children}</h1>
+  );
 }
