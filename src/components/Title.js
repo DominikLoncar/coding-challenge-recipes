a90162abfce13d3804891f75ad8b573fa0fe7151
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 00:14:28 +0100

    Added leading for the title text.

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function Title({ children = 'Lorem Ipsum' }) {
   return (
-    <h1 className="text-4xl text-midnight-blue font-dm-sans">{children}</h1>
+    <h1 className="text-4xl text-midnight-blue font-dm-sans leading-snug">
+      {children}
+    </h1>
   );
 }
