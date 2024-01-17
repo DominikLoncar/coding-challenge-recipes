82b3d8d511465863b3686f12885070aa1b648e26
Dominik Loncar <dominikloncar@outlook.com>
2024-01-17 23:41:21 +0100

    Fixed bug for the Title export.

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export { default as Logo } from './Logo';
 export { default as Navbar } from './Navbar';
 export { default as NavbarItem } from './NavbarItem';
-export { default as Title } from './TitleAndSearchBar';
+export { default as Title } from './Title';
 export { default as SearchBar } from './TitleAndSearchBar';
 export { default as TitleAndSearchBar } from './TitleAndSearchBar';
 export { default as Button } from './Button';
