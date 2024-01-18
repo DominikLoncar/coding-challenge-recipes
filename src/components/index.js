a228cc37cd1b5a8bcb83f3264f3b66585712243b
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 02:27:43 +0100

    Exported ArrowButton component.

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,7 @@ export { default as Subtitle } from './Subtitle';
 export { default as SearchBar } from './SearchBar';
 export { default as TitleAndSearchBar } from './TitleAndSearchBar';
 export { default as Button } from './Button';
+export { default as ArrowButton } from './ArrowButton';
 export { default as Hero } from './Hero';
 export { default as Card } from './Card';
 export { default as Tag } from './Tag';
