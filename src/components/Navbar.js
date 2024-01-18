58c05acf3124adada7bbdda4070342af73656ea0
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 03:54:37 +0100

    Updated navbar styling and basic icons.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,48 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import NavbarItem from './NavbarItem';
+import { GoHomeFill } from 'react-icons/go';
 
 export default function Navbar() {
   const navbarItems = [
     {
       link: 'homepage',
       text: 'Homepage',
+      icon: 'home',
     },
     {
       link: 'recipes',
       text: 'Recipes',
+      icon: 'home',
     },
     {
       link: 'community',
       text: 'Community',
+      icon: 'home',
     },
     {
       link: 'saved-recipes',
       text: 'Saved recipes',
+      icon: 'home',
     },
     {
       link: 'settings',
       text: 'Settings',
+      icon: 'home',
     },
   ];
 
+  /*export function iconGenerator(iconName) {
+    return <GoHomeFill />;
+  }*/
+
   return (
-    <div className="bg-light-gray text-dark-gray">
-      <ul className="flex flex-col gap-6">
+    <div className="bg-light-gray text-dark-gray h-full">
+      <ul className="flex flex-col gap-6 pt-8 h-full">
         {navbarItems.map(item => (
-          <NavbarItem link={item.link} text={item.text} />
+          <NavbarItem link={item.link} text={item.text}>
+            <GoHomeFill size={24} />
+          </NavbarItem>
         ))}
       </ul>
     </div>
