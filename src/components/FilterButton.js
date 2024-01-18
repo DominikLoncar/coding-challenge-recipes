1b2df689fdbdaac1226f7a063cc4a689724baebe
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 14:03:41 +0100

    Added proper props and styling.

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { FaBook } from 'react-icons/fa6';
 
-export default function FilterButton({ name }) {
+export default function FilterButton({ name = 'ButtonName' }) {
   const id = `${name.toLowerCase().replace(/\s+/g, '-')}-radio-button`;
 
   return (
-    <span>
-      <input type="radio" id={id} name="fav_language" value="Recipes"></input>
-      <label htmlFor="recipes-radio-button">Recipes</label>
+    <span className="text-sm flex items-center font-dm-sans bg-light-gray text-dark-gray border-athens-gray border-2 px-4 py-1 rounded-md active:bg-light-green active:text-white m-2 mr-0">
+      <FaBook className="text-gray m-0 p-0 mr-4" size={14} />
+      <input
+        type="radio"
+        id={id}
+        name={id}
+        value={name}
+        className="hidden"
+      ></input>
+      <label htmlFor={id}>{name}</label>
     </span>
   );
 }
