4bb0b821781b539b80e4e901058ebf9df4a3101d
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 01:37:16 +0100

    Added logic for converting completionTimeInMinutes into a formatted version.

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,17 +3,30 @@ import { StaticImage } from 'gatsby-plugin-image';
 
 export default function Card({
   title = 'Lorem Ipsum',
-  completionTimeInMinutes = 60,
+  completionTimeInMinutes = 20,
   difficulty = 1,
   tags = [],
 }) {
+  let completionTimeFormatted = '';
+
+  if (completionTimeInMinutes >= 60) {
+    completionTimeFormatted = `${Math.floor(completionTimeInMinutes / 60)}h`;
+  }
+  if (completionTimeInMinutes < 60) {
+    completionTimeFormatted = `${completionTimeInMinutes}m`;
+  } else if (completionTimeInMinutes % 60 > 0) {
+    completionTimeFormatted = `${completionTimeFormatted} ${
+      completionTimeInMinutes % 60
+    }m`;
+  }
+
   return (
     <div className="grid grid-cols-3">
       <CardImg />
       <div className="col-start-2 col-end-4 bg-slate-400 ">
         <h3 className="text-xl font-dm-sans text-midnight-blue">{title}</h3>
         <div>
-          <div>{completionTimeInMinutes}</div> <div>{difficulty}</div>
+          <div>{completionTimeFormatted}</div> <div>{difficulty}</div>
         </div>
         <div>
           {tags.map(tag => (
