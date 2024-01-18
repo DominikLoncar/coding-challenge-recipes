e05a9fb90ce89101ff4b9ed486e656e97c084e90
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 03:38:56 +0100

    Made search bar full width.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,12 +5,12 @@ export default function SearchBar({
   placeholder = 'Search for any recipes...',
 }) {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center w-full">
       <input
         type="text"
         id="search-recipes"
         placeholder={placeholder}
-        className="rounded-md border-[1px] border-ghost px-4 py-2 pr-10 text-aluminium"
+        className="rounded-md border-[1px] border-ghost px-4 py-2 pr-10 text-aluminium w-full"
       />
       <div className="-ml-8 pointer-events-none">
         <FiSearch color="#727E8D" size={16} />
--- src/components/TitleAndSearchBar.js
+++ src/components/TitleAndSearchBar.js
@@ -4,7 +4,7 @@ import SearchBar from './SearchBar';
 
 export default function TitleAndSearchBar() {
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
       <Title>Discover Recipes</Title>
       <SearchBar />
     </div>
