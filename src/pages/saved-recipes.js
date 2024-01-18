481e78ec39e16b8848b56687d64a6a6912e40f16
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 11:50:23 +0100

    Craeted SavedRecipes page.

---
+++ src/pages/saved-recipes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Layout, Title } from '../components';
+
+export default function SavedRecipes() {
+  return (
+    <Layout>
+      <Title>Saved Recipes</Title>
+    </Layout>
+  );
+}
