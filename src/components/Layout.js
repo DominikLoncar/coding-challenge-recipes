fce0b700647d6c0433be400624ea762d7362b7b8
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 14:58:40 +0100

    Desktop layout invisible on smaller screens.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,12 +4,14 @@ import { Logo, Navbar } from './index';
 export default function Layout({ children }) {
   return (
     <main>
-      <div className="grid grid-cols-6 h-full">
-        <div>
+      <div className="lg:grid grid-cols-6 h-full">
+        <div className="hidden lg:block">
           <Logo />
           <Navbar />
         </div>
-        <div className="col-start-2 col-end-7 mt-12 mx-36">{children}</div>
+        <div className="col-start-2 col-end-7 mt-12 mx-4 md:mx-12 lg:mx-14 xl:mx-20 2xl:mx-36">
+          {children}
+        </div>
       </div>
     </main>
   );
