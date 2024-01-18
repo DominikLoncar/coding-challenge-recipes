b5126c786d69689a3f74cc274c461b687f7da0dc
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 02:48:03 +0100

    Passed in tailwind styling.

--- src/components/ArrowButton.js
+++ src/components/ArrowButton.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { FaArrowRight } from 'react-icons/fa6';
 
-export default function ArrowButton({ text = 'Button', link = '' }) {
+export default function ArrowButton({
+  text = 'Button',
+  link = '',
+  className = '',
+}) {
+  const buttonClassName = `${className} font-dm-sans font-medium text-green text-lg pl-6 px-0 py-2 rounded-md hover:opacity-80 transition-hover transition-opacity`;
+
   return (
-    <button className="font-dm-sans font-medium text-green text-lg pl-6 px-0 py-2 rounded-md hover:opacity-80 transition-hover transition-opacity">
+    <button className={buttonClassName}>
       <div className="flex gap-2 items-center">
         <span>{text}</span> <FaArrowRight />
       </div>
