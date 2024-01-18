74410a2fc6df8b571624423caa7d99d23556e9e6
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 04:21:31 +0100

    Updated NavbarItem styling.

--- src/components/NavbarItem.js
+++ src/components/NavbarItem.js
@@ -8,7 +8,7 @@ export default function NavbarItem({ link = '', text = 'Lorem', children }) {
     <li className="hover:text-dark-green transition-colors">
       <Link to={linkWithSlash}>
         <div className="flex gap-4 pl-8 pt-2">
-          <span>{children}</span> <span>{text}</span>
+          <span className="opacity-50">{children}</span> <span>{text}</span>
         </div>
       </Link>
     </li>
