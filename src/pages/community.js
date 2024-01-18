04eec9855c986466ea08dd34c7ebb635706f999e
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 14:36:58 +0100

    Added Lorem Ipsum text for Community page.

--- src/pages/community.js
+++ src/pages/community.js
@@ -5,6 +5,20 @@ export default function Community() {
   return (
     <Layout>
       <Title>Community</Title>
+      <p className="font-dm-sans text-midnight-blue">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Tortor posuere ac ut
+        consequat semper viverra nam libero. Vestibulum rhoncus est pellentesque
+        elit ullamcorper dignissim cras. Eu turpis egestas pretium aenean. Vel
+        orci porta non pulvinar neque laoreet suspendisse. Risus feugiat in ante
+        metus dictum at tempor commodo ullamcorper. Sit amet mattis vulputate
+        enim nulla aliquet porttitor. Eu consequat ac felis donec et odio.
+        Consectetur a erat nam at lectus urna duis. Lorem ipsum dolor sit amet
+        consectetur adipiscing. Ultricies mi eget mauris pharetra. Facilisis
+        volutpat est velit egestas dui. Cursus mattis molestie a iaculis at
+        erat. Velit euismod in pellentesque massa placerat duis ultricies lacus
+        sed. Vel orci porta non pulvinar neque.{' '}
+      </p>
     </Layout>
   );
 }
