51a1edfbaa328c1c43beea9512012fc0c256f422
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 11:45:51 +0100

    Exported Layout component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,54 +7,47 @@ import {
   Card,
   Subtitle,
   ArrowButton,
+  Layout,
 } from '../components/index';
 
 const IndexPage = () => {
   return (
-    <main>
-      <div className="grid grid-cols-6 h-full">
-        <div>
-          <Logo />
-          <Navbar />
-        </div>
-        <div className="col-start-2 col-end-7 mt-12 mx-36">
-          <TitleAndSearchBar />
-          <Hero />
-          <div className="flex flex-auto">
-            <Subtitle>New Recipes</Subtitle>
-            <ArrowButton className="ml-auto" text="See all recipes" />
-          </div>
-          <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-6">
-            <Card
-              title="Duck Confit"
-              completionTimeInMinutes={150}
-              tags={['Miscellaneous', 'French']}
-              difficulty={2}
-            />
-            <Card
-              title="Omelette Cake"
-              completionTimeInMinutes={45}
-              tags={['Vegetarian', 'French']}
-              difficulty={2}
-            />
-            <Card
-              title="Peanut Butter Cheesecake"
-              completionTimeInMinutes={75}
-              difficulty={2}
-              tags={['Dessert', 'American']}
-            />
-            <Card
-              title="Escovitch Fish"
-              completionTimeInMinutes={90}
-              difficulty={3}
-              tags={['Vegetarian', 'French']}
-            />
-            <Card tags={['Vegetarian', 'French']} difficulty={2} />
-            <Card tags={['Vegetarian', 'French']} difficulty={2} />
-          </div>
-        </div>
+    <Layout>
+      <TitleAndSearchBar />
+      <Hero />
+      <div className="flex flex-auto">
+        <Subtitle>New Recipes</Subtitle>
+        <ArrowButton className="ml-auto" text="See all recipes" />
       </div>
-    </main>
+      <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-6">
+        <Card
+          title="Duck Confit"
+          completionTimeInMinutes={150}
+          tags={['Miscellaneous', 'French']}
+          difficulty={2}
+        />
+        <Card
+          title="Omelette Cake"
+          completionTimeInMinutes={45}
+          tags={['Vegetarian', 'French']}
+          difficulty={2}
+        />
+        <Card
+          title="Peanut Butter Cheesecake"
+          completionTimeInMinutes={75}
+          difficulty={2}
+          tags={['Dessert', 'American']}
+        />
+        <Card
+          title="Escovitch Fish"
+          completionTimeInMinutes={90}
+          difficulty={3}
+          tags={['Vegetarian', 'French']}
+        />
+        <Card tags={['Vegetarian', 'French']} difficulty={2} />
+        <Card tags={['Vegetarian', 'French']} difficulty={2} />
+      </div>
+    </Layout>
   );
 };
 
