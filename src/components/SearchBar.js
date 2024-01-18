3266af3494a75cb9ab094b0de84a3aa5d3e2f783
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 14:34:11 +0100

    Fixed SearchBar open/close issue.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,30 +9,25 @@ export default function SearchBar({
   const emptyDivRef = useRef(null);
 
   const [inputFocused, setInputFocused] = useState(false);
-  const [isOutsideClicked, setIsOutsideClicked] = useState(false);
 
   useEffect(() => {
     const handleOutsideClick = event => {
-      // Check if the clicked element is outside the div
       if (
+        inputRef.current &&
         emptyDivRef.current &&
         !emptyDivRef.current.contains(event.target) &&
         !inputRef.current.contains(event.target)
       ) {
         setInputFocused(false);
-      } else {
-        setInputFocused(true);
       }
     };
 
-    // Add event listener for click outside the div
     document.addEventListener('click', handleOutsideClick);
 
-    // Cleanup the event listener on component unmount
     return () => {
       document.removeEventListener('click', handleOutsideClick);
     };
-  }, [emptyDivRef]);
+  }, [inputRef, emptyDivRef]);
 
   useEffect(() => {
     const handleResize = () => {
@@ -55,15 +50,8 @@ export default function SearchBar({
     setInputFocused(true);
   };
 
-  const handleEmptyDivInteraction = () => {
-    setInputFocused(true);
-  };
-
-  const handleBlur = () => {
-    setInputFocused(false);
-  };
   return (
-    <div className=" w-full font-dm-sans">
+    <div className="w-full font-dm-sans">
       <div className="m-0 p-0 w-full">
         <div className="flex items-center">
           <input
@@ -72,7 +60,6 @@ export default function SearchBar({
             placeholder={placeholder}
             className="rounded-md border-[1px] border-ghost px-4 py-2 pr-10 text-midnight-blue w-full focus:border-dark-green focus:border-2 focus:shadow-inner hover:border-green outline-none"
             onClick={handleInput}
-            /*onBlur={handleBlur}*/
             ref={inputRef}
           />
           <div className="-ml-8 pointer-events-none">
@@ -83,8 +70,6 @@ export default function SearchBar({
           {inputFocused && (
             <div
               ref={emptyDivRef}
-              onClick={handleInput}
-              onMouseDown={handleEmptyDivInteraction}
               className="bg-white border-2 border-light-gray shadow-lg mt-2 min-w-64 ml-0 rounded-lg absolute z-50"
             >
               <div className="flex">
