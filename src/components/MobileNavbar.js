72e17e62f83641e1de4bb35fa1ea1e9fef487662
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 15:06:55 +0100

    Updated MobileNavbar styling and display.

--- src/components/MobileNavbar.js
+++ src/components/MobileNavbar.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function MobileNavbar() {
-  return <div>MobileNavbar</div>;
+  return (
+    <div className=" md:hidden bg-gray p-6 m-4 mb-2 rounded-full fixed bottom-0 w-[calc(100%-30px)] mr-10 z-50">
+      mobile navbar
+    </div>
+  );
 }
