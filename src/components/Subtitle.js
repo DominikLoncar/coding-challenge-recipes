b649f9ce7170b5193a1f5e94d8320bc54859f9f3
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 02:23:54 +0100

    Updated Subtitle component styling, added children prop and replaced div with h2 tag.

--- src/components/Subtitle.js
+++ src/components/Subtitle.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
-export default function Subtitle() {
-  return <div>Subtitle</div>;
+export default function Subtitle({ children = 'Lorem Ipsum' }) {
+  return (
+    <h2 className="text-3xl text-midnight-blue font-dm-sans">{children}</h2>
+  );
 }
