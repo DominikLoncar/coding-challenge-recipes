c56ab67fca7d0491ea8f3bf4ea53c718ac7a37c3
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 04:15:44 +0100

    Generated icons.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,49 +2,70 @@ import React from 'react';
 import { Link } from 'gatsby';
 import NavbarItem from './NavbarItem';
 import { GoHomeFill } from 'react-icons/go';
+import { TbSettingsFilled } from 'react-icons/tb';
+import { FaHeart } from 'react-icons/fa';
+import { MdPeopleAlt } from 'react-icons/md';
+import { FaBookOpen } from 'react-icons/fa6';
+
+import '../styles/navbar.css';
 
 export default function Navbar() {
   const navbarItems = [
     {
       link: 'homepage',
       text: 'Homepage',
-      icon: 'home',
+      iconName: 'home',
     },
     {
       link: 'recipes',
       text: 'Recipes',
-      icon: 'home',
+      iconName: 'book',
     },
     {
       link: 'community',
       text: 'Community',
-      icon: 'home',
+      iconName: 'people',
     },
     {
       link: 'saved-recipes',
       text: 'Saved recipes',
-      icon: 'home',
+      iconName: 'heart',
     },
     {
       link: 'settings',
       text: 'Settings',
-      icon: 'home',
+      iconName: 'settings',
     },
   ];
 
-  /*export function iconGenerator(iconName) {
-    return <GoHomeFill />;
-  }*/
-
   return (
-    <div className="bg-light-gray text-dark-gray h-full">
+    <div className="bg-light-gray text-dark-gray h-full navbar">
       <ul className="flex flex-col gap-6 pt-8 h-full">
         {navbarItems.map(item => (
           <NavbarItem link={item.link} text={item.text}>
-            <GoHomeFill size={24} />
+            {iconGenerator(item.iconName)}
           </NavbarItem>
         ))}
       </ul>
     </div>
   );
+}
+
+function iconGenerator(iconName = '', size = 24) {
+  switch (iconName) {
+    case 'home':
+      return <GoHomeFill size={size} />;
+    case 'book':
+      return <FaBookOpen size={size} />;
+    case 'people':
+      return <MdPeopleAlt size={size} />;
+    case 'heart':
+      return <FaHeart size={size} />;
+
+    case 'settings':
+      return <TbSettingsFilled size={size} />;
+    default:
+      return <></>;
+      break;
+  }
 }
