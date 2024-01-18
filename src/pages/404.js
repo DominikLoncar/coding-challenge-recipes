b96f5efda02392c2c1911b97866752a07157b411
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 14:47:32 +0100

    Added Lorem Ipsum for 404 and Settings page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
-import { Layout, Subtitle, Title } from '../components';
+import { Layout, Subtitle, Title, Paragraph } from '../components';
 
 export default function Recipes() {
   return (
@@ -10,6 +10,139 @@ export default function Recipes() {
       <Subtitle>
         <Link to="/">Go Home</Link>
       </Subtitle>
+
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Tortor posuere ac ut
+        consequat semper viverra nam libero. Vestibulum rhoncus est pellentesque
+        elit ullamcorper dignissim cras. Eu turpis egestas pretium aenean. Vel
+        orci porta non plit ullamcorper dignissim cras. Eu turpis egestas
+        pretium aenean. Vel orci porta non pulvinar neque laoreet suspendisse.
+        Risus feugiat in ante metus dictum at tempor commodo ullamcorper. Sit
+        amet mattis vulputate enim nulla aliquet porttitor. Eu consequat ac
+        felis donec et odio. Consectetur a erat nam at lelit ullamcorper
+        dignissim cras. Eu turpis egestas pretium aenean. Vel orci porta non
+        pulvinar neque laoreet suspendisse. Risus feugiat in ante metus dictum
+        at tempor commodo ullamcorper. Sit amet mattis vulputate enim nulla
+        aliquet porttitor. Eu consequat ac felis donec et odio. Consectetur a
+        erat nam at leulvinar neque laoreet suspendisse. Risus feugiat in ante
+        metus dictum at tempor commodo ullamcorper. Sit amet mattis vulputate
+        enim nulla aliquet porttitor. Eu consequat ac felis donec et odio.
+        Consectetur a erat nam at lectus urna duis. Lorem ipsum dolor sit amet
+        consectetur adipiscing. Ultricies mi eget mauris pharetra. Facilisis
+        volutpat est velit egestas dui. Cursus mattis molestie a iaculis at
+        erat. Velit euismod in pellentesque massa placerat duis ultricies lacus
+        sed. Vel orci porta non pulvinar neque.{' '}
+      </Paragraph>
+      <Paragraph>
+        eque laoreet suspendisse. Risus feugiat in ante metus dictum at tempor
+        commodo ullamcorper. Sit amet mattis vulputate enim nulla aliquet pi
+        eget mauris pharetra. Facilisis volutpat est velit egestas dui. Cursus
+        mattis molestie a iaculis at erat. Velit euismod in pellentesque massa
+        placerat duis ultricies lacus sed. Vel orci porta non pulvinar neque.{' '}
+      </Paragraph>
+      <Subtitle>Lorem Ipsum</Subtitle>
+      <Paragraph>
+        Lorem ipnon pulvinar neque laoreet suspendisse. Risus feugiat in
+        antetempor incididunt ut labore et dolore magna aliqua. Tortor posuere
+        ac ut consequat semper viverra nam libero. Vestibulum rhoncus est
+        pellentesque elit ullamcorper dignissim cras. Eu turpis egestas pretium
+        aenean. Vel orci porta non pulvinar neque laoreet suspendisse. Risus
+        feugiat in antetempor incididunt ut labore et dolore magna aliqua.
+        Tortor posuere ac ut consequat semper viverra nam libero. Vestibulum
+        rhoncus est pellentesque elit ullamcorper dignissim cras. Eu turpis
+        egestas pretium aenean. Vel orci porta non pulvinar neque laoreet
+        suspendisse. Risus feugiat in ante metus dictum at tempor commodo
+        ullamcorper. Sit amet mattis vulputate enim nulla aliquet porttitor. Eu
+        consequat ac felis donec et odio. Consectetur a erat nam at lectus urna
+        duis. Lorem ipsum dolor sit amet consectetur adipiscing. Ultricies mi
+        eget mauris pharetra. Facilisis volutpat est velit egestas dui. Cursus
+        mattis molestie a iaculis at erat. Velit euismod in pellentesque massa
+        placerat duis ultricies lacus sed. Vel orci porta non pulvinar neque.{' '}
+      </Paragraph>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Tortor posuere ac ut
+        consequat semlit ullamcorper dignissim cras. Eu turpis egestas pretium
+        aenean. Vel orci porta non pulvinar neque laoreet suspendisse. Risus
+        feugiat in ante metus dictum at tempor commodo ullamcorper. Sit amet
+        mattis vulputate enim nulla aliquet porttitor. Eu consequat ac felis
+        donec et odio. Consectetur a erat nam at leper viverra nam libero.
+        Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Eu
+        turpis egestas pretium aenean. Vel orci porta non pulvi Consectetur a
+        erat nam at lectus urna duis. Lorem ipsum dolor sit amet consectetur
+        adipiscing. Ultricies mi eget mauris pharetra. Facilisis volutpat est
+        velit egestas dui. Cursus mattis molestie a iaculis at erat. Velit
+        euismod in pellentesque massa placerat duis ultricies lacus sed. Vel
+        orci porta non pulvinar neque.{' '}
+      </Paragraph>
+      <Paragraph>
+        ipsum dolor viverra nam libero. Vestibulum rhoncus est pellentesque elit
+        ullamcorper dignissim cras. Eu turpis egestas pretium aenean. Vel orci
+        porta no Ultricies mi eget mauris pharetra. Facilisis volutpat est velit
+        egestas dui. Cursus mattis molestie a iaculis at erat. Velit euismod in
+        pellentesque massa placerat duis ultricies lacus sed. Vel orci porta non
+        pulvinar neque.{' '}
+      </Paragraph>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididuntlit ullamcorper dignissim cras. Eu turpis egestas
+        pretium aenean. Vel orci porta non pulvinar neque laoreet suspendisse.
+        Risus feugiat in ante metus dictum at tempor commodo ullamcorper. Sit
+        amet mattis vulputate enim nulla aliquet porttitor. Eu consequat ac
+        felis donec et odio. Consectetur a erat nam at le ac ut consequat semper
+        viverra nam libero. Vestibulum rhoncus est pellentesque elit ullamcorper
+        dignissim cras. Eu turpis egestas pretium aenean. Vel orci porta non
+        pulvinar neque laoreet suspendisse. Risus feugiat in anteim cras. Eu
+        turpis egestas pretium aenean. Vel orci porta non pulvinar neque laoreet
+        suspendisse. Risus feugiat in ante metus dictum at tempor commodo
+        ullamcorper. Sit amet mattis vulputate enim nulla aliquet porttitor. Eu
+        consequat ac felis donec et odio. Consectetur a erat nam at lectus urna
+        duis. Lorem ipsum dolor sit amet consectetur adipiscing. Ultricies mi
+        eget mauris pharetra. Facilisis volutpat est velit egestas dui. Cursus
+        mattis molestie a iaculis at erat. Velit euismod in pellentesque
+        mastempor incididunt ut labore et dolore magna aliqua. Tortor posuere ac
+        ut consequat semper viverra nam libero. Vestibulum rhoncus est
+        pellentesque elit ullamcorper dignissim cras. Eu turpis egestas pretium
+        aenean. Vel orci porta non pulvinar neque laoreet suspendisse. Risus
+        feugiat in antetempor incididunt ut labore et dolore magna aliqua.
+        Tortor posuere ac ut consequat semper viverra nam libero. Vestibulum
+        rhoncus est pellentesque elit ullamcorper dignissim cras. Eu turpis
+        egestas pretium aenean. Vel orci porta non pulvinar neque laoreet
+        suspendisse. Risus feugiat in antetempor incididunt ut labore et dolore
+        magna aliqua. Tortor posuere ac ut consequat semper viverra nam libero.
+        Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Eu
+        turpis egestas pretium aenean. Vel orci porta non pulvinar neque laoreet
+        suspendisse. Risus feugiat in ante
+      </Paragraph>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        et suspendisse. Risus feugiat in ante metus dictum at tempor commodo
+        ullamcorper. Sit amet mattis vulputate enim nulla aliquet porttitor. Eu
+        consequat ac felis donec et odio. Consectetur a erat nam at lectus urna
+        duis. Lorem ipsum dolor sit amet consectetur adipiscing. Ultricies mi
+        eget mauris pharetra. Facilisis volutpat est velit egestas dui. Cursus
+        mattis molestie a iaculis at erat. Velit euismod in pellentesque massa
+        placerat duis ultricies lacus sed. Vel orci porta non pulvinar neque.{' '}
+      </Paragraph>
+      <Paragraph>
+        Lorem ips posuere ac ut consequat semper viverra nam libero. Vestibulum
+        rhoncus est pellentesque elit ullamcorper dignissim cras. Eu turpis
+        egestas pretium aenean. Vel orci porta non pulvinar neque laoreet
+        suspendisse. Risus feugiat in antetempor incididunt ut labore et dolore
+        magna aliqua. Tortor posuere ac ut consequat semper viverra nam libero.
+        Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Eu
+        turpis egestas pretium aenean. Vel orci porta non pulvinar neque laoreet
+        suspendisse. Risus feugiat in antecras. Eu turpis egestas pretium
+        aenean. Vel orci porta non pulvinar neque laoreet suspendisse. Risus
+        feugiat in ante metus dictum at tempor commodo ullamcorper. Sit amet
+        mattis vulputate enim nulla aliquet porttitor. Eu consequat ac felis
+        donec et odio. Consectetur a erat nam at lectus urna duis. Lorem ipsum
+        dolor sit amet consectetur adipiscing. Ultricies mi eget mauris
+        pharetra. Facilisis volutpat est velit egestas dui. Cursus mattis
+        molestie a iaculis at erat. Velit euismod in pellentesque massa placerat
+        duis ultricies lacus sed. Vel orci porta non pulvinar neque.{' '}
+      </Paragraph>
     </Layout>
   );
 }
--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,10 +1,110 @@
 import React from 'react';
-import { Layout, Title } from '../components';
+import { Layout, Title, Paragraph, Subtitle } from '../components';
 
 export default function Settings() {
   return (
     <Layout>
       <Title>Settings</Title>
+      <Paragraph>
+        is vulputate enim nulla aliquet porttitor. Eu consequat ac felis donec
+        et odio. Consectetur a erat nam at lelit ullamcorper dignissim cras. Eu
+        turpis egestas pretium aenean. Vel orci porta non pulvinar neque laoreet
+        suspendisse. Risus feugiat in ante metus dictum at tempor commodo
+        ullamcorper. Sit amet mattis vulputate enim nulla alique porttitor. Eu
+        consequat ac felis donec et odio. Consectetur a erat nam at leulvinar
+        neque laoreet suspendisse. Risus feugiat in ante metus dictum at tempor
+        commodo ullamcorper. Sit amet mattis vulputate enim nulla adipiscing.
+        Ultricies mi eget mauris pharetra. Facilisis volutpat est velit egestas
+        dui. Cursus mattis molestie a iaculis at erat. Velit euismod in
+        pellentesque massa placerat duis ultricies lacus sed. Vel orci porta non
+        pulvinar neque.{' '}
+      </Paragraph>
+      <Paragraph>
+        eque laoreepharetra. Facilisis volutpat est velit egestas dui. Cursus
+        mattis molestie a iaculis at erat. Velit euismod in pellentesque massa
+        placerat duis ultricies lacus sed. Vel orci porta non pulvinar neque.{' '}
+      </Paragraph>
+      <Subtitle>Lorem Ipsum</Subtitle>
+      <Paragraph>
+        Lorem ipnon pulvinar neque laoreet suspendisse. Risus feugiat in
+        antetempor incididunt ut labore et dolore magna aliqua. Tortor posuere
+        ac ut consequat semper viverra nam libero. Vestibulum rhoncus est
+        pellentesque elit ullamcorper dignissim cras. Eu turpis egestas pretium
+        aenean. Vel orci porta non pulvinar neque laoreet suspendisse. Risus
+        feugiat in antetempor incididunt ut labore et dolore magna aliqua.
+        Tortor posuere ac ut consequat semper viverra nam libero. Vestibulum
+        rhoncus estSit amet mattis vulputate enim nulla aliquet porttitor. Eu
+        consequat ac felis donec et odio. Consectetur a erat nam at lectus urna
+        duis. Lorem ipsum dolor sit amet consectetur adipiscing. Ultricies mi
+        eget mauris pharetra. Facilisis volutpat est velit egestas dui. Cursus
+        mattis molestie a iaculis at erat. Velit euismod in pellentesque massa
+        placerat duis ultricies lacus sed. Vel orci porta non pulvinar neque.{' '}
+      </Paragraph>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incistas pretium aenean. Vel orci porta non pulvi Consectetur a
+        erat nam at lectus urna duis. Lorem ipsum dolor sit amet consectetur
+        adipiscing. UltriCursus mattis molestie a iaculis at erat. Velit euismod
+        in pellentesque massa placerat duis ultricies lacus sed. Vel orci porta
+        non pulvinar neque.{' '}
+      </Paragraph>
+      <Paragraph>
+        ipsum dolor viverra nam libero. Vestibulum rhoncus est pellentesque elit
+        ullamcorper dignissimtis molestie a iaculis at erat. Velit euismod in
+        pellentesque massa placerat duis ultricies lacus sed. Vel orci porta non
+        pulvinar neque.{' '}
+      </Paragraph>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        im cras. Eu turpis egestas pretium aenean. Vel orci porta non pulvinar
+        neque laoreet suspendisse. Risus feugiat in anteim cras. Eu turpis
+        egestas pretium aenean. Vel orci porta non pulvinar neque laoreet
+        suspendisse. Risus feugiat in ante metus dictum at tempor commodo
+        ullamcorper. Sit amet mattis vulputate enim nulla aliquet porttitor. Eu
+        consequat ac felis donec et odio. Consectetur a erat nam at lectus urna
+        duis. Lorem ipsum dolor sit amet consectetur adipiscing. Ultricies mi
+        eget mauris pharetra. Facilisis volutpat est velit egestas dui. Cursus
+        mattis molestie a iaculis at erat. Velit euismod in pellentesque
+        mastempor incididunt ut labore et dolore magna aliqua. Tortor posuere ac
+        ut consequat semper viverra nam libero. Vestibulum rhoncus est
+        pellentesque elit ullamcorper dignissim cras. Eu turpis egestas pretium
+        aenean. Vel orci porta non pulvinar neque laoreet suspendisse. Risus
+        feugiat in antetempor incididunt ut labore et dolore magna aliqua.
+        Tortor posuere ac ut consequat semper viverra nam libero. Vestibulum
+        rhoncus est pellentesque elit ullamcorper dignissim cras. Eu turpis
+        egestas pretium aenean. Vel orci porta non pulvinar neque laoreet
+        suspendisse. Risus feugiat in antetempor incididunt ut labore et dolore
+        magna aliqua. Tortor posuere ac ut consequat semper viverra nam libero.
+        Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Eu
+        turpis egestas pretium aenean. Vel orci porta non pulvinar neque laoreet
+        suspendisse. Risus feugiat in ante
+      </Paragraph>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        et suspendisse. Risus feugiat in ante metus dictum at tempor commodo
+        ullamcorper. Sit amet mattis vulputate enim nulla aliquet porttitor. Eu
+        consequat ac felis donec et odio. Consectetur a erat nam at lectus urna
+        duis. Lorem ipsum dolor sit amet consectetur adipiscing. Ultricies mi
+        eget mauris pharetra. Facilisis volutpat est velit egestas dui. Cursus
+        mattis molestie a iaculis at erat. Velit euismod in pellentesque massa
+        placerat duis ultricies lacus sed. Vel orci porta non pulvinar neque.{' '}
+      </Paragraph>
+      <Paragraph>
+        Lorem ips posuere ac ut consequat semper viverra nam libero. Vestibulum
+        rhoncus est pellentesque elit ullamcorper dignissim cras. Eu turpis
+        egestas pretium aenean. Vel orci porta non pulvinar neque laoreet cus
+        est pellentesque elit ullamcorper dignissim cras. Eu turpis egestas
+        pretium aenean. Vel orci porta non pulvinar neque laoreet suspendisse.
+        Risus feugiat in antecras. Eu turpis egestas pretium aenean. Vel orci
+        porta non pulvinar neque laoreet suspendisse. Risus feugiat in ante
+        metus dictum at tempor commodo ullamcorper. Sit amet mattis vulputate
+        enim nulla aliquet porttitor. Eu consequat ac felis donec et odio.
+        Consectetur a erat nam at lectus urna duis. Lorem ipsum dolor sit amet
+        consectetur adipiscing. Ultricies mi eget mauris pharetra. Facilisis
+        volutpat est velit egestas dui. Cursus mattis molestie a iaculis at
+        erat. Velit euismod in pellentesque massa placerat duis ultricies lacus
+        sed. Vel orci porta non pulvinar neque.{' '}
+      </Paragraph>
     </Layout>
   );
 }
