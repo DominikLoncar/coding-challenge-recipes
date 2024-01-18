33961a4a5803cd1fe26d9fd742a837dc3e730548
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 11:46:42 +0100

    Created Recipes page.

---
+++ src/pages/recipes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Layout, Title } from '../components';
+
+export default function Recipes() {
+  return (
+    <Layout>
+      <Title>Recipes</Title>
+    </Layout>
+  );
+}
