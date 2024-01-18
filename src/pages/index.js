f80c2aa1eae28e8549da49bf42fd45929878f11f
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 02:26:16 +0100

    Imported and displayed Subtitle component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import {
   TitleAndSearchBar,
   Hero,
   Card,
+  Subtitle,
 } from '../components/index';
 
 const IndexPage = () => {
@@ -18,6 +19,7 @@ const IndexPage = () => {
         <div className="col-start-2 col-end-7 mt-12 mx-36">
           <TitleAndSearchBar />
           <Hero />
+          <Subtitle>New Recipes</Subtitle>
           <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-6">
             <Card tags={['Vegetarian', 'French']} difficulty={2} />
             <Card tags={['Vegetarian', 'French']} difficulty={2} />
