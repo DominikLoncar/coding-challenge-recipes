4d0fb2429fb8377d22e0cbd84f1f287eee1c341f
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 12:52:06 +0100

    Updated SearchBar state and interaction logic.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 
 export default function SearchBar({
@@ -6,6 +6,8 @@ export default function SearchBar({
 }) {
   const inputRef = useRef(null);
   const emptyDivRef = useRef(null);
+
+  const [inputFocused, setInputFocused] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -22,10 +24,19 @@ export default function SearchBar({
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, []);
+  }, [inputFocused]);
 
-  const handleInput = () => {};
+  const handleInput = () => {
+    setInputFocused(true);
+  };
 
+  const handleEmptyDivInteraction = () => {
+    setInputFocused(true);
+  };
+
+  const handleBlur = () => {
+    setInputFocused(false);
+  };
   return (
     <div className=" w-full">
       <div className="m-0 p-0 w-full">
@@ -36,6 +47,7 @@ export default function SearchBar({
             placeholder={placeholder}
             className="rounded-md border-[1px] border-ghost px-4 py-2 pr-10 text-midnight-blue w-full focus:border-dark-green focus:border-2 focus:shadow-inner hover:border-green outline-none"
             onClick={handleInput}
+            onBlur={handleBlur}
             ref={inputRef}
           />
           <div className="-ml-8 pointer-events-none">
@@ -43,10 +55,13 @@ export default function SearchBar({
           </div>
         </div>
         <div className="w-full">
-          <div
-            className="bg-gray h-32 mt-2 min-w-64 ml-0 rounded-lg absolute z-50 "
-            ref={emptyDivRef}
-          ></div>
+          {inputFocused && (
+            <div
+              ref={emptyDivRef}
+              onMouseDown={handleEmptyDivInteraction}
+              className="bg-gray h-32 mt-2 min-w-64 ml-0 rounded-lg absolute z-50"
+            ></div>
+          )}
         </div>
       </div>
     </div>
