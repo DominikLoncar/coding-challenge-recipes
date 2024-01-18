aca804a35af0c701825524fa5a8f30e9fd22b39f
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 02:04:23 +0100

    Updated Tag component styling. Changed from div to span.

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-export default function Tag({ name }) {
-  return <div>{name}</div>;
+export default function Tag({ children = 'Lorem' }) {
+  return (
+    <span className="mr-1 font-dm-sans font-medium px-2 py-0.5 text-dark-blue bg-light-blue rounded-md">
+      {children}
+    </span>
+  );
 }
