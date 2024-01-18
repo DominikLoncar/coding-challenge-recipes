643aba509d43074bf5472b8993e50fc5c5a00206
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 11:31:16 +0100

    Updated input styling on hover and focus.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,13 +4,16 @@ import { FiSearch } from 'react-icons/fi';
 export default function SearchBar({
   placeholder = 'Search for any recipes...',
 }) {
+  const handleInput = e => e.preventDefault;
+
   return (
     <div className="flex items-center w-full">
       <input
         type="text"
         id="search-recipes"
         placeholder={placeholder}
-        className="rounded-md border-[1px] border-ghost px-4 py-2 pr-10 text-aluminium w-full"
+        className="rounded-md border-[1px] border-ghost px-4 py-2 pr-10 text-aluminium w-full focus:border-dark-green focus:border-2 hover:border-green outline-none"
+        onClick={handleInput}
       />
       <div className="-ml-8 pointer-events-none">
         <FiSearch color="#727E8D" size={16} />
