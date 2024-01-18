c3b93788d0e524c55ca3ff0b238afca5bfa47adb
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 02:36:35 +0100

    Added react-icons. Imported and displayed right arrow (FaArrowRight).

--- src/components/ArrowButton.js
+++ src/components/ArrowButton.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { FaArrowRight } from 'react-icons/fa6';
 
 export default function ArrowButton({ text = 'Button', link = '' }) {
   return (
     <button className="font-dm-sans font-medium text-green text-lg px-6 py-2 rounded-md hover:opacity-80 transition-hover transition-opacity">
-      {text}
+      {text} <FaArrowRight />
     </button>
   );
 }
