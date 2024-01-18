c0dcf4f784a605e451f3128abe0ccabd2bec33e9
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 14:40:09 +0100

    Added vertical margin.

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default function Paragraph({ children }) {
-  return <p className="font-dm-sans text-midnight-blue text-md">{children}</p>;
+  return (
+    <p className="font-dm-sans text-midnight-blue text-md my-8">{children}</p>
+  );
 }
