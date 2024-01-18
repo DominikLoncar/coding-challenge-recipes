a5641198a136b01eeb9e475bcd45b18be2a4ff5c
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 03:54:55 +0100

    Updated NavbarItem styling.

--- src/components/NavbarItem.js
+++ src/components/NavbarItem.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Children } from 'react';
 import { Link } from 'gatsby';
 
-export default function NavbarItem({ link = '', text = 'Lorem' }) {
+export default function NavbarItem({ link = '', text = 'Lorem', children }) {
   const linkWithSlash = `/${link}`;
 
   return (
     <li className="hover:text-dark-green">
-      <Link to={linkWithSlash}>{text}</Link>
+      <Link to={linkWithSlash}>
+        <div className="flex gap-4 pl-8 pt-2">
+          <span>{children}</span> <span>{text}</span>
+        </div>
+      </Link>
     </li>
   );
 }
