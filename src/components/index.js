b36cdd5aab4c8507bae2257cc282268304e586ab
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 11:46:02 +0100

    Exported Layout component.

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as Layout } from './Layout';
 export { default as Logo } from './Logo';
 export { default as Navbar } from './Navbar';
 export { default as NavbarItem } from './NavbarItem';
