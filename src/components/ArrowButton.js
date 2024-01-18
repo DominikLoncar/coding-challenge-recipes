ac3d9f6a737322105b622c56479b18188ad96143
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 14:03:29 +0100

    Updated Link.

--- src/components/ArrowButton.js
+++ src/components/ArrowButton.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { FaArrowRight } from 'react-icons/fa6';
+import { Link } from 'gatsby';
 
 export default function ArrowButton({
   text = 'Button',
-  link = '',
+  link = '/',
   className = '',
 }) {
   const buttonClassName = `${className} font-dm-sans font-medium text-green text-lg pl-6 px-0 py-2 rounded-md hover:opacity-80 transition-hover transition-opacity pb-4`;
 
   return (
-    <button className={buttonClassName}>
+    <Link className={buttonClassName} to={link}>
       <div className="flex gap-2 items-center">
         <span>{text}</span> <FaArrowRight />
       </div>
-    </button>
+    </Link>
   );
 }
