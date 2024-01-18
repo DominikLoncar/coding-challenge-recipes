30acfc98a77b2914d80c8f4417a912dc45246be4
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 15:16:06 +0100

    Updated NavbarItem.

--- src/components/NavbarItem.js
+++ src/components/NavbarItem.js
@@ -1,14 +1,15 @@
 import React, { Children } from 'react';
 import { Link } from 'gatsby';
 
-export default function NavbarItem({ link = '', text = 'Lorem', children }) {
+export default function NavbarItem({ link = '', text = '', children }) {
   const linkWithSlash = `/${link}`;
 
   return (
     <li className="hover:text-dark-green transition-colors font-dm-sans font-medium">
       <Link to={linkWithSlash}>
         <div className="flex gap-4 pl-8 pt-2">
-          <span className="opacity-50">{children}</span> <span>{text}</span>
+          <span className="opacity-50">{children} </span>{' '}
+          {text && <span>{text}</span>}
         </div>
       </Link>
     </li>
