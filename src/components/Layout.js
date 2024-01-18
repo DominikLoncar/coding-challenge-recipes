d1ecb1107596d3cf0780fa4da896da47baebd4a7
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 11:45:39 +0100

    Updated Layout component.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Logo, Navbar } from './index';
 
 export default function Layout({ children }) {
-  return <div>{children}</div>;
+  return (
+    <main>
+      <div className="grid grid-cols-6 h-full">
+        <div>
+          <Logo />
+          <Navbar />
+        </div>
+        <div className="col-start-2 col-end-7 mt-12 mx-36">{children}</div>
+      </div>
+    </main>
+  );
 }
