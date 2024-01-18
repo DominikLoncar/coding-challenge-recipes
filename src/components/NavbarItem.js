872fe67b471027f6d302a0e692c2caaf2f94b135
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 04:22:14 +0100

    Updated font.

--- src/components/NavbarItem.js
+++ src/components/NavbarItem.js
@@ -5,7 +5,7 @@ export default function NavbarItem({ link = '', text = 'Lorem', children }) {
   const linkWithSlash = `/${link}`;
 
   return (
-    <li className="hover:text-dark-green transition-colors">
+    <li className="hover:text-dark-green transition-colors font-dm-sans font-medium">
       <Link to={linkWithSlash}>
         <div className="flex gap-4 pl-8 pt-2">
           <span className="opacity-50">{children}</span> <span>{text}</span>
