f56663945f41cbc3dce19231ebd020335cb87fa8
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 02:26:30 +0100

    Updated Subtitle component.

--- src/components/Subtitle.js
+++ src/components/Subtitle.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function Subtitle({ children = 'Lorem Ipsum' }) {
   return (
-    <h2 className="text-3xl text-midnight-blue font-dm-sans">{children}</h2>
+    <h2 className="text-3xl text-midnight-blue font-dm-sans mb-7">
+      {children}
+    </h2>
   );
 }
