b777182d36a7b1fd91e5c5a62fb0b51467c597e5
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 11:40:24 +0100

    Updated Tag hover styling.

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Tag({ children = 'Lorem' }) {
   return (
-    <span className="mr-1 font-dm-sans font-medium px-2 py-0.5 text-dark-blue bg-light-blue rounded-md">
+    <span className="mr-1 font-dm-sans font-medium px-2 py-0.5 text-dark-blue bg-light-blue rounded-md hover:opacity-80 cursor-pointer transition-opacity">
       {children}
     </span>
   );
