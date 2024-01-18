aed8c35582f63f116df0a3a684d89fd52ab18149
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 02:34:28 +0100

    Updated ArrowButton styling.

--- src/components/ArrowButton.js
+++ src/components/ArrowButton.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-export default function ArrowButton() {
-  return <div>ArrowButton</div>;
+export default function ArrowButton({ text = 'Button', link = '' }) {
+  return (
+    <button className="font-dm-sans font-medium text-green text-lg px-6 py-2 rounded-md hover:opacity-80 transition-hover transition-opacity">
+      {text}
+    </button>
+  );
 }
