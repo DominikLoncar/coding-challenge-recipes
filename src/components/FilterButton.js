97dff9c9b4883ced271b9f2dfe5a3306f7355c83
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 13:22:38 +0100

    Created regex function for adding dashes between id words if there are any. Made it lowercase as well.

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-export default function FilterButton() {
+export default function FilterButton({ name }) {
+  const id = `${name.toLowerCase().replace(/\s+/g, '-')}-radio-button`;
+
   return (
     <span>
-      <input
-        type="radio"
-        id="recipes-radio-button"
-        name="fav_language"
-        value="Recipes"
-      ></input>
+      <input type="radio" id={id} name="fav_language" value="Recipes"></input>
       <label htmlFor="recipes-radio-button">Recipes</label>
     </span>
   );
