8b9223005c98f7a924b82107f46da7e136152fa6
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 04:16:12 +0100

    Added color transition animation.

--- src/components/NavbarItem.js
+++ src/components/NavbarItem.js
@@ -5,7 +5,7 @@ export default function NavbarItem({ link = '', text = 'Lorem', children }) {
   const linkWithSlash = `/${link}`;
 
   return (
-    <li className="hover:text-dark-green">
+    <li className="hover:text-dark-green transition-colors">
       <Link to={linkWithSlash}>
         <div className="flex gap-4 pl-8 pt-2">
           <span>{children}</span> <span>{text}</span>
