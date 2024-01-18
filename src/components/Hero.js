a42787ebeb3eccc52f05bdf3c3b8f2c3542d4463
Dominik Loncar <dominikloncar@outlook.com>
2024-01-18 03:38:44 +0100

    Added more styling for Hero.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import { StaticImage } from 'gatsby-plugin-image';
 export default function Hero() {
   return (
     <div>
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 bg-black-haze rounded-lg px-12 pt-10 pb-12 mt-10 mb-40">
         <div>
           <Title>
             <strong>
@@ -25,7 +25,7 @@ export default function Hero() {
 
 export function HeroImg() {
   return (
-    <div className="max-w-96">
+    <div className="max-w-96 absolute right-44 -mt-10">
       <StaticImage
         src="../images/hero-img.png"
         alt="A meal representation for the hero section"
